b31a58fd453d6d480c056702e866523d8ae24def
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2021-11-21 00:08:59 +0530

    stack practice

---
+++ Day-3/code.js
@@ -0,0 +1,36 @@
+ var arr = []
+//  var n =5;
+function push1 (n,x){
+   last =-1;
+    if(last ==n){
+        console.log("Stack Overflow")
+    }else {
+        // arr.push(x);
+        // console.log("nice")
+        // console.log(arr)
+        last =last+1;
+        arr[last] = 10;
+        last++;
+        arr[last] = 20;
+        last++;
+        arr[last] = 30;
+        last++;
+        arr[last] = 14;
+        last++;
+        arr[last] = 100;
+        last++;
+        arr[last] = 10;
+        last++;
+        arr[last] = 10;
+        last++;
+
+
+        console.log(arr)
+    }
+    console.log(arr.length)
+
+};
+
+
+push1(5,0);
+
